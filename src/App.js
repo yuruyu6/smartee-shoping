25a7b590b71b38903c373e21fa07187f426cced3
yuruyu6 <yuruyu6@gmail.com>
2021-07-05 01:27:32 +0300

    Prepare app for deployment

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Header from './components/Header/Header';
 import MainSection from './components/MainSection/MainSection';
 
 function App() {
   return (
     <div className="App bg-gray-100 min-h-screen">
-      <BrowserRouter>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <Header />
         <MainSection />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
