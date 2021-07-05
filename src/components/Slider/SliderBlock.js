690835c5f0d0df50a04eb078901329e615f02a58
yuruyu6 <yuruyu6@gmail.com>
2021-07-05 21:56:46 +0300

    Fix SliderBlock bug when items count < 4

--- src/components/Slider/SliderBlock.js
+++ src/components/Slider/SliderBlock.js
@@ -10,37 +10,6 @@ import NextArrow from './NextArrow';
 import PrevArrow from './PrevArrow';
 import DataNotFound from '../UI/DataNotFound';
 
-const sliderSettings = {
-  nextArrow: <NextArrow />,
-  prevArrow: <PrevArrow />,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  initialSlide: 0,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-  ],
-};
-
 const DEFAULT_CATEGORY = 'cpu';
 
 export default function SliderBlock() {
@@ -49,6 +18,38 @@ export default function SliderBlock() {
   const isLoaded = useSelector(({ productsGroup }) => productsGroup.isLoaded);
   const categories = useSelector(({ categories }) => categories.categories);
   const [currentCategory, setCurrentCategory] = useState(DEFAULT_CATEGORY);
+
+  const sliderSettings = {
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    infinite: products.length > 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
   useEffect(() => {
     dispatch(fetchCategories());
